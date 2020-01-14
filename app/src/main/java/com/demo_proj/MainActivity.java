b15f823c6bb9169e7cd62e466f842153a9f908d7
Vaibhav <vaibhavvadnere@gmail.com>
2020-01-14 13:10:34 -0800

    Main Activity changes

--- app/src/main/java/com/demo_proj/MainActivity.java
+++ app/src/main/java/com/demo_proj/MainActivity.java
@@ -1,15 +1,23 @@
 package com.demo_proj;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String test = "";
+    String newtest = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+
+        test = "test";
+        newtest = "new Test";
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
